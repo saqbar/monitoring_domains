70d748e9d9cccb27731555b326bfada8285257f3
sqbar <you@example.com>
2025-12-29 10:22:09 +0200

    edit dublicat files

--- src/telegram/enteties/chanel_tg.entity.ts
+++
@@ -1,9 +0,0 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
-@Entity('chanel_tg')
-export class ChanelTg {
-  @PrimaryGeneratedColumn()
-  id: number;
-  @Column({ type: 'text' })
-  channel_id: string;
-}
--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -3,7 +3,6 @@ import { TelegramController } from './controllers/telegram.controller';
 import { TelegramService } from './services/telegram.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SessionEntity } from './enteties/session.entity';
-import { ChanelTg } from './enteties/chanel_tg.entity';
 import { TelegramHelperService } from './services/telegram-helper.service';
 import { ChannelTgService } from './services/channels-tg.service';
 import { ChannelTgEntity } from './enteties/channels.entity';
@@ -12,7 +11,7 @@ import { ChannelTgEntity } from './enteties/channels.entity';
   controllers: [TelegramController],
   providers: [TelegramService, TelegramHelperService, ChannelTgService],
   imports: [
-    TypeOrmModule.forFeature([SessionEntity, ChanelTg, ChannelTgEntity]),
+    TypeOrmModule.forFeature([SessionEntity, ChannelTgEntity]),
   ],
   exports: [TelegramHelperService, ChannelTgService],
 })
